91ec033c1f3902dae863282131606905b47d6d37
Kevin <jkwk539@gmail.com>
2022-11-21 00:30:22 -0500

    .

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,16 +15,20 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between bg-gray-700 w-full p-4'>
+      <Link to='/signin'>
       <h1 className='text-center text-2xl font-bold'>
         Stackathon
       </h1>
+      </Link>
       {user?.displayName ? (
+        <Link to='/'>
         <button onClick={handleSignOut} className="transition duration-500 ease-in-out 
         bg-gray-500 hover:bg-green-600 transform 
         hover:-translate-y-1 hover:scale-110 
         rounded-lg p-4 border border-green-900">
           Logout
         </button>
+        </Link>
       ) : (
         <Link to='/signin'>Sign in</Link>
       )}
--- src/pages.js/home.js
+++ src/pages.js/home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       <h1 className='text-center text-3xl font-bold py-8'>Home Page</h1>
+      <Link to='/signin'>
+        <h2 className='text-center text-3xl font-bold py-8'>Click Me to Sign In!
+        </h2>
+      </Link>
     </div>
   )
 }
